d0c234579dee72791d83e03c2f8c69881616f082
timtraversy <timtrav@gmail.com>
2020-11-16 19:14:54 +0000

    Working trello client dependancy

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/timtraversy/trello-to-github
 
 go 1.15
+
+require github.com/adlio/trello v1.8.0
---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetBoard(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
